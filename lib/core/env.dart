ffc4fc777e687ade3bf45d9d2797be67b7604c04
benik <galstyan.benik@gmail.com>
2024-11-18 22:52:23 +0200

    implemented firebase core

---
+++ lib/core/env.dart
@@ -0,0 +1,4 @@
+abstract class Env {
+  static const String androidApiKey = 'AIzaSyD4nqg-BvK5K9o4eCY9HUCSfVOYibxcI9c';
+  static const String iosApiKey = 'AIzaSyDj8uvzb-zy5n7RvB0VeeNFyEecNeGDDSA';
+}
