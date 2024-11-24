688bfd4301a0a03b19b0e3ccce48635af0e349a6
galst <galstyan.benik@gmail.com>
2024-11-24 20:54:58 +0200

    implement main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_meb/data/service/storage_service/firestore_service.dart';
 import 'package:todo_meb/firebase_options.dart';
+import 'package:todo_meb/presentation/providers/task_form_provider.dart';
+import 'package:todo_meb/presentation/providers/task_list_provider.dart';
 import 'package:todo_meb/todo_meb_app.dart';
 import 'package:todo_meb/service_locator.dart';
 import 'package:flutter/material.dart';
@@ -8,5 +12,18 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupServicesLocator();
-  runApp(TodoMebApp());
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => TaskFormProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => TaskListProvider(getIt<FirestoreService>()),
+        ),
+      ],
+      child: TodoMebApp(),
+    ),
+  );
 }
